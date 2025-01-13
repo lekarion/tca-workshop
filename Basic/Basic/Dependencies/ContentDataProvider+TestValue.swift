ee7b6ccf841c1e429f4deff55c564bf787beaab8
irondev-mpw <lekarion@macpaw.com>
2025-01-13 14:22:13 +0200

    feat: simplest unit test implementation

--- Basic/Basic/Dependencies/ContentDataProvider+TestValue.swift
+++ Basic/Basic/Dependencies/ContentDataProvider+TestValue.swift
@@ -10,6 +10,50 @@ import Dependencies
 
 extension ContentDataProvider: TestDependencyKey {
     static var testValue: ContentDataProvider {
-        .liveValue
+        .init {
+            "980"
+        } pushFromCurrency: { _ in
+        } fetchCurrencies: {
+            Self.mockedData.currencies
+        } pushCurrencies: { _ in
+        } fetchCurrencyRecords: {
+            Self.mockedData.records
+        } pushCurrencyRecords: { _ in
+        }
     }
+
+    static var previewValue: ContentDataProvider {
+        .testValue
+    }
+}
+
+private extension ContentDataProvider {
+    struct TestDataStruct {
+        var currencies: [Currency]
+        var records: [Currency: CurrencyRecord]
+    }
+    static let mockedData = {
+        let decoder = JSONDecoder()
+
+        var currencies: [Currency]?
+        let currenciesURL = Bundle.main.url(forResource: "testCurrencies", withExtension: "json")
+        if
+            let currenciesURL,
+            let data = try? Data(contentsOf: currenciesURL) {
+            currencies = try? decoder.decode([Currency].self, from: data)
+        }
+
+        var records: [Currency: CurrencyRecord]?
+        let recordsURL = Bundle.main.url(forResource: "testCurrencyRecords", withExtension: "json")
+        if
+            let recordsURL,
+            let data = try? Data(contentsOf: recordsURL) {
+            records = try? decoder.decode([Currency: CurrencyRecord].self, from: data)
+        }
+
+        return TestDataStruct(
+            currencies: currencies ?? [],
+            records: records ?? [:]
+        )
+    }()
 }
--- Basic/BasicTests/BasicTests.swift
+++ Basic/BasicTests/BasicTests.swift
@@ -6,12 +6,33 @@
 //
 
 import Testing
+import ComposableArchitecture
 @testable import Basic
 
 struct BasicTests {
+    @Test func testAppInitReset() async throws {
+        let store = await TestStore(initialState: AppReducer.State()) {
+            AppReducer()
+        }
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        await store.send(.initialize)
+        await store.receive(\.contentFeature)
+        await store.receive(\.setInitialized) {
+            $0.isInitialized = true
+        }
+        await store.skipReceivedActions()
+
+        await store.send(.resetContent)
+        await store.receive(\.contentFeature)
+        await store.skipReceivedActions()
     }
 
+    @Test func testContentInit() async throws {
+        let store = await TestStore(initialState: ContentReducer.State()) {
+            ContentReducer()
+        }
+
+        await store.send(.initialize)
+        await store.skipReceivedActions()
+    }
 }
